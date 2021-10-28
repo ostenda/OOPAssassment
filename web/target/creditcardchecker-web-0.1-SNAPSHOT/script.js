c293b5cca032cb11ff02619d2f51ef2ab50e7d60
ostenda <ostenda95@gmail.com>
2021-10-28 21:22:35 +0100

    fist commit

---
+++ web/target/creditcardchecker-web-0.1-SNAPSHOT/script.js
@@ -0,0 +1,9 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+
+
+// This function display values
+function display(value) {
+    document.getElementById("result").value += value;
+}
